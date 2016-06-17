26ed2d4e289a004b7fce1dfaced3e5aed8bc2278
Tyler Romeo <tyler.romeo@willowtreeapps.com>
2016-06-16 20:45:37 -0400

    rename User model. Add fields for compliments sent and received

---
+++ src/main/java/org/romeo/compliments/domain/User.java
@@ -0,0 +1,74 @@
+package org.romeo.compliments.domain;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.annotations.ApiModelProperty;
+
+/**
+ * User: tylerromeo
+ * Date: 6/15/16
+ * Time: 8:32 PM
+ */
+public class User {
+
+    private String id;
+    private String email;
+    private String imageUrl;
+    private int complimentsSent;
+    private int complimentsReceived;
+
+    public User(String email, String id, String imageUrl, int complimentsSent, int complimentsReceived) {
+        this.email = email;
+        this.id = id;
+        this.imageUrl = imageUrl;
+        this.complimentsSent = complimentsSent;
+        this.complimentsReceived = complimentsReceived;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    @JsonProperty(required = true)
+    @ApiModelProperty(notes = "The user's email address", required = true)
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @JsonProperty(required = false)
+    @ApiModelProperty(notes = "A url pointing to an image of the user", required = false)
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    @JsonProperty(required = true, defaultValue = "0")
+    @ApiModelProperty(notes = "The numer of compliments this user has sent", required = true)
+    public int getComplimentsSent() {
+        return complimentsSent;
+    }
+
+    public void setComplimentsSent(int complimentsSent) {
+        this.complimentsSent = complimentsSent;
+    }
+
+    @JsonProperty(required = true, defaultValue = "0")
+    @ApiModelProperty(notes = "The numer of compliments this user has received", required = true)
+    public int getComplimentsReceived() {
+        return complimentsReceived;
+    }
+
+    public void setComplimentsReceived(int complimentsReceived) {
+        this.complimentsReceived = complimentsReceived;
+    }
+}
--- src/main/java/org/romeo/compliments/web/UserController.java
+++ src/main/java/org/romeo/compliments/web/UserController.java
@@ -1,7 +1,7 @@
 package org.romeo.compliments.web;
 
 import io.swagger.annotations.*;
-import org.romeo.compliments.domain.Model;
+import org.romeo.compliments.domain.User;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -22,11 +22,11 @@ public class UserController {
             @ApiImplicitParam(name = "id", value = "User's id", required = true, dataType = "string", paramType = "path")
     })
     @ApiResponses(value = {
-            @ApiResponse(code = 200, message = "Success", response = Model.class),
+            @ApiResponse(code = 200, message = "Success", response = User.class),
             @ApiResponse(code = 401, message = "Unauthorized"),
             @ApiResponse(code = 404, message = "Not Found"),
             @ApiResponse(code = 500, message = "Failure")})
-    public Model getById(@PathVariable("id") String id) {
-        return new Model(id, "Tyler@example.com", "http://placekitten.com/400/200");
+    public User getById(@PathVariable("id") String id) {
+        return new User(id, "Tyler@example.com", "http://placekitten.com/400/200", 0, 0);
     }
 }
