2ce8ef19ac2504dae645aab05e33f2686f57bb6c
Tyler Romeo <tyler.romeo@willowtreeapps.com>
2016-06-16 21:57:22 -0400

    fix failing unit test
    spring test application context wasn't loading web resources

--- src/test/java/org/romeo/compliments/MainApplicationTests.java
+++ src/test/java/org/romeo/compliments/MainApplicationTests.java
@@ -3,10 +3,12 @@ package org.romeo.compliments;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.WebIntegrationTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = MainApplication.class)
+@WebIntegrationTest
 public class MainApplicationTests {
 
 	@Test
