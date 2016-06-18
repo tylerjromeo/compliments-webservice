54bcc590fdf67c4274444873478704a73e190df0
Tyler Romeo <tyler.romeo@willowtreeapps.com>
2016-06-18 19:24:33 -0400

    add test for get user by id when the resourse isn't found

--- src/test/java/org/romeo/compliments/web/UserControllerTest.java
+++ src/test/java/org/romeo/compliments/web/UserControllerTest.java
@@ -9,12 +9,16 @@ import org.mockito.MockitoAnnotations;
 import org.romeo.compliments.MainApplication;
 import org.romeo.compliments.persistence.UserRepository;
 import org.romeo.compliments.web.domain.User;
+import org.romeo.compliments.web.exception.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.boot.test.WebIntegrationTest;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;;
-import static org.junit.Assert.*;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.*;
+
+;
 
 /**
  * User: tylerromeo
@@ -51,6 +55,15 @@ public class UserControllerTest {
         when(userRepository.findOne(id)).thenReturn(testUser);
         User result = userController.getById(id);
         assertEquals(result, User.fromDbUser(testUser));
+        verify(userRepository).findOne(id);
+    }
+
+    @Test(expected = ResourceNotFoundException.class)
+    public void testGetById_IdNotFound() throws Exception {
+        long id = 12341234;
+        when(userRepository.findOne(id)).thenReturn(null);
+        userController.getById(id);
+        verify(userRepository).findAll();
         verify(userRepository).findOne(id);
     }
 }
