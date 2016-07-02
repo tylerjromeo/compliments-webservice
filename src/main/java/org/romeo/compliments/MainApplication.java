f4d00b39d75c558032154e8f520188801e012c51
Tyler Romeo <tyler.romeo@willowtreeapps.com>
2016-07-01 22:13:54 -0400

    separate out oauth client configuration to be more explicit

--- src/main/java/org/romeo/compliments/MainApplication.java
+++ src/main/java/org/romeo/compliments/MainApplication.java
@@ -1,27 +1,46 @@
 package org.romeo.compliments;
 
-import static springfox.documentation.builders.PathSelectors.regex;
-
 import org.romeo.compliments.persistence.UserRepository;
 import org.romeo.compliments.persistence.domain.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.ResourceServerProperties;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.UserInfoTokenServices;
+import org.springframework.boot.context.embedded.FilterRegistrationBean;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.client.OAuth2ClientContext;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.security.oauth2.client.filter.OAuth2ClientAuthenticationProcessingFilter;
+import org.springframework.security.oauth2.client.filter.OAuth2ClientContextFilter;
+import org.springframework.security.oauth2.client.resource.OAuth2ProtectedResourceDetails;
+import org.springframework.security.oauth2.client.token.grant.code.AuthorizationCodeResourceDetails;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import javax.servlet.Filter;
 import java.util.ArrayList;
 import java.util.List;
 
+import static springfox.documentation.builders.PathSelectors.regex;
+
 @SpringBootApplication
 @EnableSwagger2
-@EnableOAuth2Sso
-public class MainApplication {
+@EnableOAuth2Client
+//@EnableAuthorizationServer
+public class MainApplication extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    OAuth2ClientContext oauth2ClientContext;
 
     public static void main(String[] args) {
         ApplicationContext cxt = SpringApplication.run(MainApplication.class, args);
@@ -37,6 +56,52 @@ public class MainApplication {
         users.add(new User("Tyler Romeo", "tyler.romeo@example.com", "http://placekitten.com/300/200"));
         users.add(new User("Kevin Welcher", "kevin.welcher@example.com", "http://placekitten.com/200/200"));
         userRepository.save(users);
+    }
+
+    //TODO extract security crap to its own class
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .antMatcher("/**")
+                .addFilterBefore(ssoFilter(), BasicAuthenticationFilter.class)
+                .authorizeRequests()
+//                    .antMatchers("/", "/login**")
+//                    .permitAll()
+                .anyRequest()
+                    .authenticated();
+                //TODO: add login and logout to swagger somehow
+                //TODO: this line will add a logout token requiring a csrf token. Figure out how this works with elm, or even if it's needed in the first place
+                //.and().logout().logoutSuccessUrl("/").permitAll();
+    }
+
+    private Filter ssoFilter() {
+        OAuth2ClientAuthenticationProcessingFilter googleFilter = new OAuth2ClientAuthenticationProcessingFilter("/login");
+        OAuth2RestTemplate googleTemplate = new OAuth2RestTemplate(google(), oauth2ClientContext);
+        googleFilter.setRestTemplate(googleTemplate);
+        googleFilter.setTokenServices(new UserInfoTokenServices(googleResource().getUserInfoUri(), google().getClientId()));
+        return googleFilter;
+    }
+
+    @Bean
+    @ConfigurationProperties("google.client")
+    OAuth2ProtectedResourceDetails google() {
+        return new AuthorizationCodeResourceDetails();
+    }
+
+    @Bean
+    @ConfigurationProperties("google.resource")
+    ResourceServerProperties googleResource() {
+        return new ResourceServerProperties();
+    }
+
+    @Bean
+    public FilterRegistrationBean oauth2ClientFilterRegistration(
+            OAuth2ClientContextFilter filter) {
+        FilterRegistrationBean registration = new FilterRegistrationBean();
+        registration.setFilter(filter);
+        registration.setOrder(-100);
+        return registration;
     }
 
     @Bean
